8c62ac9f905e2ee51b4cfada5f4c7e7e05054160
reza samkhanian <rezasamkhanian@yahoo.com>
2019-02-14 10:07:38 +0330

    changing

--- Components/node.js
+++ Components/node.js
@@ -40,8 +40,15 @@ export default class Node extends React.PureComponent {
 			<g {...this.props.gProps} transform={this.getTransform()} onContextMenu={this.handleRightClick} onClick={this.handleClick}>
 				<circle {...this.props.circleProps} r={this.props.radius} style={{fill: this.props.color ? this.props.color : '#35B995'}}/>
 				<text {...this.props.textProps} dx={this.props.radius - 13.5} dy={this.props.offset - 13}>
-					{this.props[this.props.labelProp]}
-					
+					<React.Fragment>
+						{this.props[this.props.labelProp]}
+						{
+							this.props.status &&
+						(this.props.status === 'TODO' ? <Todo/> :
+							(this.props.status === 'DOING' ? <Timelapse/> : <Check/>)
+						)
+						}
+					</React.Fragment>
 				</text>
 			</g>);
 	}
--- dist/index.js
+++ dist/index.js
@@ -28,7 +28,7 @@
 			: (global.ReactTreeGraph = factory(
 					global.PropTypes,
 					global.React,
-					global.CheckBoxOutlineBlank,
+					global.Todo,
 					global.Check,
 					global.Timelapse,
 					null,
@@ -40,7 +40,7 @@
 })(this, function(
 	PropTypes,
 	React,
-	CheckBoxOutlineBlank,
+	Todo,
 	Check,
 	Timelapse,
 	find,
@@ -324,7 +324,17 @@
 									dx: this.props.radius - 13.5,
 									dy: this.props.offset - 13
 								}),
-								this.props[this.props.labelProp]
+								React.createElement(
+									React.Fragment,
+									null,
+									this.props[this.props.labelProp],
+									this.props.status &&
+										(this.props.status === 'TODO'
+											? React.createElement(Todo, null)
+											: this.props.status === 'DOING'
+												? React.createElement(Timelapse, null)
+												: React.createElement(Check, null))
+								)
 							)
 						);
 					}
